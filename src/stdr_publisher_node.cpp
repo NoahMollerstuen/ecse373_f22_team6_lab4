50da75d085494c8e4a44ea05b12b8ae9adf54665
seongmin <sxj754@case.edu>
2022-10-14 16:33:47 -0400

    avoid walls

--- src/stdr_publisher_node.cpp
+++ src/stdr_publisher_node.cpp
@@ -6,87 +6,77 @@
 #include <sstream>
 
 ros::Publisher *p_pub;
+sensor_msgs::LaserScan currentLaserMsg;
+float global_min;
+geometry_msgs::Twist des_vel;
+geometry_msgs::Twist cmd_vel;
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
-
-void cmd_velCallback(const geometry_msgs::Twist::ConstPtr& msg)
+void desiredVelCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
-  ROS_INFO("I heard");
-  p_pub->publish(msg);
+  des_vel = *msg;  // Read input value from rqt_gui as des_vel
+}
+
+void laserScanCallback(const sensor_msgs::LaserScan::ConstPtr& msg)
+{
+  currentLaserMsg = *msg;
+  float min = 4.0;
+  float max = 0.0;
+  int maxindex;
+
+  for (int i=0; i<20; i++)
+  {
+    if (min > currentLaserMsg.ranges[125 + i])
+    {
+      min = currentLaserMsg.ranges[125 + i];
+    }
+    if (max < currentLaserMsg.ranges[125 + i])
+    {
+      max = currentLaserMsg.ranges[125 + i];
+      maxindex = i;
+    }
+  }
+
+  global_min = min;
 }
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
   ros::init(argc, argv, "talker");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle n;
 
-  /**
-   * The advertise() function is how you tell ROS that you want to
-   * publish on a given topic name. This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing. After this advertise() call is made, the master
-   * node will notify anyone who is trying to subscribe to this topic name,
-   * and they will in turn negotiate a peer-to-peer connection with this
-   * node.  advertise() returns a Publisher object which allows you to
-   * publish messages on that topic through a call to publish().  Once
-   * all copies of the returned Publisher object are destroyed, the topic
-   * will be automatically unadvertised.
-   *
-   * The second parameter to advertise() is the size of the message queue
-   * used for publishing messages.  If messages are published more quickly
-   * than we can send them, the number here specifies how many messages to
-   * buffer up before throwing some away.
-   */
   ros::Publisher cmd_vel_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
-  ros::Subscriber sub = n.subscribe("cmd_vel", 1000, cmd_velCallback);
+  ros::Publisher log_pub = n.advertise<std_msgs::String>("mylog", 1000);
+  
+  ros::Subscriber sub = n.subscribe("des_vel", 1000, desiredVelCallback);
+  ros::Subscriber lasersub = n.subscribe("laser_1", 1000, laserScanCallback);
   
   p_pub = &cmd_vel_pub;
 
   ros::Rate loop_rate(10);
 
-  /**
-   * A count of how many messages we have sent. This is used to create
-   * a unique string for each message.
-   */
-  int count = 0;
   while (ros::ok())
   {
-    /**
-     * This is a message object. You stuff it with data, and then publish it.
-     */
-    geometry_msgs::Twist cmd_vel;
+    std_msgs::String log;
+    log.data = std::to_string(global_min);
 
-    cmd_vel.linear.x = 0;
-    cmd_vel.linear.y = 0;
-    cmd_vel.linear.z = 0;
-    cmd_vel.angular.x = 0;
-    cmd_vel.angular.y = 0;
-    cmd_vel.angular.z = 0.1;
+    if (global_min > 2) {
+      cmd_vel.linear.x = des_vel.linear.x;
+      cmd_vel.angular.z = des_vel.angular.z;
+    }
+    else {
+      cmd_vel.linear.x = 0;
+      cmd_vel.angular.z = 0.5;
+    }
+
+    log_pub.publish(log);
+
 
     cmd_vel_pub.publish(cmd_vel);
 
     ros::spinOnce();
 
     loop_rate.sleep();
-    ++count;
   }
 
 
